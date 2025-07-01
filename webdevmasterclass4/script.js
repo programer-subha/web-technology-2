5a23371c3f7b41e2806a4c9da7dce231a18442a8
programer-subha <147228526+programer-subha@users.noreply.github.com>
2025-07-01 13:16:14 +0530

    Add files via upload

---
+++ webdevmasterclass4/script.js
@@ -0,0 +1,197 @@
+
+let x='<h1> welcome To biswas company</h1>'
+ document.write(x);
+
+// document.write('<h1> biswas company</h1>');
+// document.write('<h1> biswas company</h1>');
+// document.write('<h1> biswas company</h1>');
+// document.write('<h1> biswas company</h1>');
+// document.write('<h1> biswas company</h1>');
+// document.write('<h1> biswas company</h1>');
+// document.write('<h1> biswas company</h1>');
+// document.write('<h1> biswas company</h1>');
+// let y=10;
+// document.write(y);
+// var z='hello'
+// var z='subha'
+// document.write(z);
+// var e='hello';
+// e="world";
+// document.write(e);
+// let age=20;
+// age=25;
+// document.write(age);
+// const dy=78;
+// const dy=25;
+// const Age=56;
+//console.log('yfyhf');
+//document.write(<input type="text" placeholder="Enter your name"></input>)
+//number
+
+// let a =1000.89;
+// // string
+// let b ="biswas company";
+// let c ='biswas company';
+// let d ="1000.89";
+// let e ="50";
+// //bolen
+// let f = true;
+// let g = false;
+// //undifind
+// let h =undefined;
+// let i;
+// //null
+// let j= null;
+// let age =50;
+// age = null;
+// let email ='tochyanroy@gimal.com';
+// email=null;
+
+// let obj = {
+//     name:'subha giri',
+//     age:20,
+//     issingle: true
+// }
+// console.log(obj.name);
+// let x = document.getElementById("heading").innerHTML;
+// console.log(x);
+// let y= document.quarryselector('#heading').innerHTML;
+// console.log(y);
+
+
+// // console.log("hello world");
+
+
+//et x = document.getElementById('heading');
+//console.log(x)
+
+// console.log(x);
+// let y=document.getElementsByClassName('cls');
+
+// let z= document.getElementsByTagName('p');
+// console.log(z);
+// let v = document.querySelector('.cls');
+// console.log(x);
+// let x =document.querySelectorall
+
+// console.log("Hello world");
+// let a = 10
+// console.log(a)
+// console.log("hello")
+// let b =30;
+// console.log(b);
+// console.log('subha');
+//document.getElementById("box").innerHTML = <p>biswas company</p>//
+// document.getElementById('heading').textContent = "chyan"
+// document.getElementById('box').getElementsByid =<p>biswas</p>
+// document.getElementById('box').getElementsByid.outerhtml
+// document.getElementById('heading').style.color= 'red';
+//document.getElementById('box').innerHTML = "<h1 id='heading'>biswas company</h1>"
+//document.getElementById('heading').style. backgroundcolor = "green"
+//let x =document.getElementById('paragraph');
+//console.log(x);
+//console.log('subha');
+//document.getElementById('box').innerHTML='<p> new content</p>';
+//document.getElementById('container').innerHTML = "<h1>javasript class</h1>";
+//document.getElementById('heading').outerHTML = "<p>oindil sir</p>";
+//document.getElementById('heading').innerText = "oindil sir";
+//document.getElementById('heading').style.color ="yellow";
+//document.getElementById("box").innerHTML = '<h1 id ="heading">javascript</h1>';
+
+//let button = document.getElementById('btn');
+///function Tagchange(){
+   // document.getElementById ('heading').outerHTML = '<div id ="box"></div>';
+
+//btn.addEventListener('click',Tagchange);
+
+//let btn1 = document.getElementById('btn1');
+//function changecontent(){
+   // document.getElementById('paragraph')
+//}
+
+//let button1 =document.getElementById('btn1')
+// Task 1:tag change/replace Tag
+// let button = document.getElementById('btn');
+
+// function Tagchange() {
+//     document.getElementById('heading1').outerHTML ="<div id ='box'></div>";
+// }
+// Button.addEventListener('click',Tagchange);
+
+// // task 2: create Tag
+// let button1 =document.getElementById('btn1');
+// function tagCreate(){
+//     document.getElementById('box').innerHTML = "<p id ='paragraph'>chyan</p>";
+
+// }
+// button.addEventListener('click',tagCreate);
+
+// // Task 3: change content
+// let button2 =document.getElementById('btn2');
+// function changeContent(){
+//     document.getElementById('paragraph').textcontent = "sanjoy babu"
+// }
+// button2.addEventListener('click',changecontent);
+
+// // Task 4: change style
+// let button3 = document.getElementById('btn3');
+// function changestyle(){
+//     document.getElementById('paragraph').style.color="blue";
+// }
+
+// button3.addEventListener('click',stylechange);
+// let button1 =document.getElementById('btn');
+// let button2 =document.getElementById('btn');
+// let button3 =document.getElementById('btn');
+// let button4 =document.getElementById('btn');
+
+//function 1
+ function changeTag (){
+     document.getElementById('heading').outerHTML = '<h3 id ="heading">Bivash sir</h3>';
+ }
+ btn1.addEventListener('click',changeTag);
+//function 2
+ function changeContent(){
+     document.getElementById('heading').innerHTML = "Rahul Babu";
+ }
+ btn2.addEventListener('click',changeContent);
+//function 3
+ function changestyle(){
+     document.getElementById('heading').style.fontSize = "100px";
+ }
+ btn3.addEventListener('click',changestyle);
+ //function 4
+ function changeTagOance(){
+     document.getElementById('heading').outerHTML ='<p id="heading">buddhadeb</p>';
+ }
+ btn4.addEventListener('click',changeTagOance);
+
+ 
+
+  
+  
+  
+// function changestyle () {
+//     document.getElementById('heading').style.fontSize ="50px";
+//     document.getElementById('heading').style.color ="green";
+
+// }
+//button.addEventListener('click',changestyle);
+
+
+
+
+
+
+
+// let btn = document.getElementById('btn');
+
+// function showData () {
+//     let val = document.getElementById('inputTag').value;
+
+//     document.getElementById('heading').textContent =val;
+
+//    document.getElementById('heading').style.fontSize ='50px';
+//     console.log(val);
+// }
+// btn.addEventListener('click',showData);
